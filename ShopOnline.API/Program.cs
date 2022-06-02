677c1a9278b63f5762864f56bf97372f0779ba39
MarkSchrauwen <63715524+MarkSchrauwen@users.noreply.github.com>
2022-06-02 13:39:42 +0200

    Added read of all Products
    Added Get Items retrieving all products coupled to ProductCategory

--- ShopOnline.API/Program.cs
+++ ShopOnline.API/Program.cs
@@ -1,6 +1,8 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using ShopOnline.Api.Data;
+using ShopOnline.Api.Repositories;
+using ShopOnline.Api.Repositories.Contracts;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -13,6 +15,8 @@ builder.Services.AddSwaggerGen();
 
 builder.Services.AddDbContextPool<ShopOnlineDbContext>(options 
     => options.UseSqlServer(builder.Configuration.GetConnectionString("ShopOnlineConnection")));
+
+builder.Services.AddScoped<IProductRepository, ProductRepository>();
 
 var app = builder.Build();
 
